bd0b3b5d188367c520f1584ae35a31df9b89bc97
mauroSoliz98 <maurososoliz@gmail.com>
2024-09-17 18:24:14 -0400

    Mi primer commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let cantidadValores = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasenna = document.getElementById('contrasena');
+const candenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+
+const Generar = () => {
+    let nuemeroDigitado = parseInt(cantidadValores.value)
+    let password = ''
+    console.log(nuemeroDigitado);
+    
+    if( nuemeroDigitado < 8 ) {
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+    }
+    for( let i=0; i < nuemeroDigitado; i++ ){
+        let caracterAleatorio = candenaCaracteres[Math.floor(Math.random() * candenaCaracteres.length)];
+        password+=caracterAleatorio;
+    }
+    contrasenna.value = password;   
+}
+const Limpiar = () => {
+    contrasenna.value = ''
+}
